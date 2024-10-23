a762f3847f40ead613109809c909cd47713a3b80
Aaron.Wiens <aaron.wiens@student.ufv.ca>
2024-10-23 09:34:12 -0700

    .

---
+++ delete1Week.py
@@ -0,0 +1,13 @@
+import os
+import time
+import sys
+
+path = " "
+now = time.time()
+
+for f in os.listdir(path):
+    if os.stat(f).st_mtime < now - 7 * 86400:
+        if os.path.isfile(f):
+            os.remove(os.path.join(path,f))
+
+#https://stackoverflow.com/questions/12485666/python-deleting-all-files-in-a-folder-older-than-x-days
