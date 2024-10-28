55abe4b6d9b9224c47ba045b8cb2517a4f9a845f
Rajveer.Singh <rajveer.singh@student.ufv.ca>
2024-10-28 08:15:17 -0700

    Did part I of the project. Added exception handling.

--- delete1Week.py
+++ delete1Week.py
@@ -9,8 +9,17 @@ import sys
 
 zipped_dir = os.path.expanduser("~/course_project/zipped_logs")
 log_dir = os.path.expanduser("~/course_project/log")
-os.makedirs(zipped_dir, exist_ok=True)
-os.makedirs(log_dir, exist_ok=True)
+
+try:
+    os.makedirs(zipped_dir, exist_ok=True)
+    os.makedirs(log_dir, exist_ok=True)
+except OSError as e:
+    sys.stderr.write(f"Operating System error creating {zipped_dir} or {log_dir}: {e}")
+    sys.exit(1)
+except Exception as e:
+    sys.stderr.write(f"Error during {zipped_dir} or {log_dir} creation: {e}")
+    sys.exit(1)
+
 
 logging.basicConfig(
     filename=f"{log_dir}/log_rotation.log",
@@ -31,9 +40,12 @@ def delete_old_zips():
                 logging.info(f"Deleted old zip file: {file_path}")
 
         logging.info(f"Deleted {deleted_files_count} old zipped files older than one week.")
+    except OSError as e:
+        logging.error(f"Operating System error during deletion of old zipped files: {e}")
+        sys.exit(1)
     except Exception as e:
         logging.error(f"Error during deletion of old zipped files: {e}")
-        return 1
-    return 0
+        sys.exit(1)
+    sys.exit(0)
 
 #https://stackoverflow.com/questions/12485666/python-deleting-all-files-in-a-folder-older-than-x-days
